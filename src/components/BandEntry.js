1b9ca79ca3b4c416db790c5477ad96a7b64abe4a
tlammi <tangojulietlima@gmail.com>
2021-07-07 19:53:04 +0300

    Add link to band entries

--- src/components/BandEntry.js
+++ src/components/BandEntry.js
@@ -3,6 +3,7 @@ import { Grid, makeStyles } from "@material-ui/core";
 import placeHolder from "../resources/band_placeholder.png";
 
 
+const defaultLink = "https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1";
 
 const useStyles = makeStyles({
     img: {
@@ -14,12 +15,20 @@ const useStyles = makeStyles({
 
     props:
         xs: xs forwarded to material-ui
+        img: image shown in the item. Default is a placeholder
+        link: Link where the image points to. A default is used if not specified.
 */
 function BandEntry(props) {
     const classes = useStyles();
+
+    const link = props.link ? props.link : defaultLink;
+    const img = props.img ? props.img : placeHolder;
+
     return (
         <Grid item container justifyContent="center" xs={props.xs}>
-            <img src={placeHolder} className={classes.img} />
+            <a href={link}>
+                <img src={img} className={classes.img} />
+            </a>
         </Grid>
     )
 }
