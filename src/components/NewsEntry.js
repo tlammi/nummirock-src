90c44769cad11d06e637998b2730cd7816387258
tlammi <tangojulietlima@gmail.com>
2021-07-15 20:54:07 +0300

    Separated collapsable and normal news entry

--- src/components/NewsEntry.js
+++ src/components/NewsEntry.js
@@ -1,9 +1,11 @@
 
-import { Grid } from '@material-ui/core';
+import { Button, Grid, Collapse } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import newsPlaceholder from '../resources/news_placeholder.md';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
 
 const SHORT_TEXT_LIMIT = 100;
@@ -13,7 +15,6 @@ const useStyles = makeStyles({
         animation: `$fade-in 3s forwards`,
         opacity: 0,
     },
-
     "@keyframes fade-in": {
         "0%": {
             opacity: 0
@@ -21,35 +22,28 @@ const useStyles = makeStyles({
         "100%": {
             opacity: 1
         }
-    }
+    },
 });
+
 /**
  * Represents a news entry in the HTML.
  * 
  * props:
  *  url - URL to the markdown file represented 
-    brief - If true, shorten too long text, otherwise show the whole text.
-            Default is false
     delay - Set animation delay
  */
 function NewsEntry(props) {
     const [md, setMd] = useState("This is news");
-    const url = props.url ? props.url : newsPlaceholder;
-    const brief = props.brief ? props.brief : false;
-    const delay = props.delay ? props.delay : "0s";
 
-    const maybe_shorten = (text) => {
-        if (brief && text.length >= SHORT_TEXT_LIMIT) {
-            return text.substr(0, SHORT_TEXT_LIMIT) + "...";
-        }
-        return text;
-    };
+    const url = props.url ? props.url : newsPlaceholder;
+    const delay = props.delay ? props.delay : "0s";
 
     const classes = useStyles();
 
     fetch(url)
         .then(r => r.text())
-        .then(text => setMd(maybe_shorten(text)));
+        .then(text => setMd(text));
+
     return (
         <Grid item xs={12} sm={6} md={4} lg={3} className={classes.root}
             style={{
@@ -60,6 +54,53 @@ function NewsEntry(props) {
             </ReactMarkdown>
         </Grid>
     );
+}
+
+/**
+ * Represents a news entry in the HTML.
+ * 
+ * props:
+ *  url - URL to the markdown file represented 
+    delay - Set animation delay
+ */
+function ExpandableNewsEntry(props) {
+    const [brief, setBrief] = useState(true);
+
+    const [start, setStart] = useState("");
+    const [end, setEnd] = useState("");
+
+    const url = props.url ? props.url : newsPlaceholder;
+    const delay = props.delay ? props.delay : "0s";
+
+    const split = (text) => {
+        return [text.substr(0, SHORT_TEXT_LIMIT), text.substr(SHORT_TEXT_LIMIT)];
+    };
+
+    const classes = useStyles();
+    const buttonIcon = (brief ? <ExpandMoreIcon /> : <ExpandLessIcon />);
+
+    fetch(url)
+        .then(r => r.text())
+        .then(text => {
+            const [a, b] = split(text);
+            setStart(a);
+            setEnd(b);
+        });
+
+    return (
+        <Grid item xs={12} sm={6} md={4} lg={3} className={classes.root}
+            style={{
+                animationDelay: delay
+            }}>
+            <Collapse in={!brief} collapsedSize="145px">
+                <ReactMarkdown>
+                    {start + end}
+                </ReactMarkdown>
+            </Collapse>
+            <Button onClick={() => setBrief(!brief)}
+                style={{ color: "white" }}>{buttonIcon}</Button>
+        </Grid>
+    );
 };
 
-export default NewsEntry;
+export { NewsEntry, ExpandableNewsEntry };
--- src/components/NewsPreview.js
+++ src/components/NewsPreview.js
@@ -1,12 +1,11 @@
 import { Grid, makeStyles } from "@material-ui/core";
-import NewsEntry from "./NewsEntry";
+import { NewsEntry, ExpandableNewsEntry } from "./NewsEntry";
 
 const useStyles = makeStyles({
     root: {
         backgroundColor: "black",
         color: "white",
         minHeight: "100vh",
-        alignItems: "center"
     }
 }
 );
@@ -18,9 +17,9 @@ function NewsPreview() {
         <Grid container className={classes.root}>
             <Grid item xs={1}></Grid>
             <Grid container item xs={10} spacing={2} justifyContent="center">
-                <NewsEntry brief={true} delay="0.2s" />
-                <NewsEntry brief={true} delay="0.4s" />
-                <NewsEntry brief={true} delay="0.6s" />
+                <ExpandableNewsEntry delay="0.2s" />
+                <ExpandableNewsEntry delay="0.4s" />
+                <ExpandableNewsEntry delay="0.6s" />
             </Grid>
             <Grid item xs={1}></Grid>
         </Grid>
