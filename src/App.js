a2a33cef48661dd7b3a5debd5fdd6ce53657d660
tlammi <tangojulietlima@gmail.com>
2021-07-15 15:45:20 +0300

    Simple react-router

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomeIcon from '@material-ui/icons/Home';
 import LandingSection from './components/LandingSection'
 import BandPreview from './components/BandPreview';
 import NewsPreview from './components/NewsPreview';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 function HeaderBar() {
   return (
@@ -13,16 +14,16 @@ function HeaderBar() {
       <Toolbar>
         <Button><HomeIcon /></Button>
         <div style={{ flex: 1 }}></div>
-        <Button>Uutiset</Button>
-        <Button>Ohjelma</Button>
-        <Button>Info</Button>
+        <Button href="/#/uutiset">Uutiset</Button>
+        <Button href="/#/ohjelma">Ohjelma</Button>
+        <Button href="/#/info">Info</Button>
       </Toolbar>
     </AppBar>
   );
 }
 
 // Left-right alignment
-function Sections() {
+function HomePage() {
   return (
     <div>
       <HeaderBar />
@@ -35,9 +36,54 @@ function Sections() {
   );
 }
 
+function NewsPage() {
+  return (
+    <div>
+      This is news
+    </div>
+  )
+}
+
+function BandsPage() {
+  return (
+    <div>
+      This is bands.
+    </div>
+  );
+}
+
+function InfoPage() {
+  return (
+    <div>
+      This is info.
+    </div>
+  );
+}
+
+function Pages() {
+  return (
+    <HashRouter>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/uutiset">
+          <NewsPage />
+        </Route>
+        <Route exact path="/ohjelma">
+          <BandsPage />
+        </Route>
+        <Route exact path="/info">
+          <InfoPage />
+        </Route>
+      </Switch>
+    </HashRouter>
+  );
+}
+
 function App() {
   return (
-    <Sections />
+    <Pages />
   );
 }
 
