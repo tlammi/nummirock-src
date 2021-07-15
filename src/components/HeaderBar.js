24220d8079b0b3a0d5a19779da74baf794d83daa
tlammi <tangojulietlima@gmail.com>
2021-07-15 17:16:04 +0300

    Nicer toolbar

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,20 +1,45 @@
-import { AppBar, Button, Toolbar } from "@material-ui/core"
+import { AppBar, Button, Icon, Toolbar } from "@material-ui/core"
+import { makeStyles } from "@material-ui/styles";
 
-import HomeIcon from "@material-ui/icons/Home";
+const nummiLogo = "https://www.nummirock.fi/2017/images/Otsikon_grafiikat_Alasivu_1_2021.png";
 
-
+const useStyles = makeStyles({
+    root: {
+        backgroundColor: "black",
+        color: "white"
+    }
+});
 function HeaderBar() {
     // environment variable for overriding the service path
     const url = process.env.PUBLIC_URL;
+    const classes = useStyles();
+
+    const texts = [
+        "Uutiset",
+        "Ohjelma",
+        "Info",
+    ];
+
+    const components = texts.map((txt, idx) => {
+        const lower = txt.toLowerCase();
+        return (
+            <Button
+                href={url + "/#/" + lower}
+                className={classes.root}>
+                {txt}
+            </Button>)
+    });
 
     return (
-        <AppBar position="sticky">
+        <AppBar position="sticky" className={classes.root}>
             <Toolbar>
-                <Button href={url + "/"}><HomeIcon /></Button>
+                <Button href={url + "/"}>
+                    <img src={nummiLogo} style={{
+                        height: "32px"
+                    }} />
+                </Button>
                 <div style={{ flex: 1 }}></div>
-                <Button href={url + "/#/uutiset"}>Uutiset</Button>
-                <Button href={url + "/#/ohjelma"}>Ohjelma</Button>
-                <Button href={url + "/#/info"}>Info</Button>
+                {components}
             </Toolbar>
         </AppBar>
     );
