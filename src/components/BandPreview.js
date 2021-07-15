3fa5d4a2f8f536c5e323f5d6515af07dacc49cf0
tlammi <tangojulietlima@gmail.com>
2021-07-15 17:21:30 +0300

    Added sample band images

--- src/components/BandPreview.js
+++ src/components/BandPreview.js
@@ -16,10 +16,10 @@ function BandPreview() {
         <Grid item container className={classes.root} direction="row" justifyContent="space-between">
             <Grid item xs={1}></Grid>
             <Grid item container xs={10} justifyContent="center" spacing={4}>
-                <BandEntry delay="0.2s"></BandEntry>
-                <BandEntry delay="0.4s"></BandEntry>
-                <BandEntry delay="0.6s"></BandEntry>
-                <BandEntry delay="0.8s"></BandEntry>
+                <BandEntry delay="0.2s" img="https://nummirock.fi/2017/images/bandit_2021/WEB_VIRTUAL-69eyes.png"></BandEntry>
+                <BandEntry delay="0.4s" img="https://nummirock.fi/2017/images/bandit_2021/WEB_VIRTUAL-Ensiferum.png"></BandEntry>
+                <BandEntry delay="0.6s" img="https://nummirock.fi/2017/images/bandit_2021/WEB_VIRTUAL-FearOfDomination.png"></BandEntry>
+                <BandEntry delay="0.8s" img="https://nummirock.fi/2017/images/bandit_2021/WEB_VIRTUAL-NooraLouhimoExperience.png"></BandEntry>
             </Grid>
             <Grid item xs={1}></Grid>
         </Grid>
