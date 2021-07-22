c53235dd595bf900f45e0ca781c024d58bb6cbdd
tlammi <tangojulietlima@gmail.com>
2021-07-22 22:01:42 +0300

    Added text for the landing section

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -3,18 +3,23 @@ import Grid from '../util/Grid';
 import Img from '../util/Img';
 
 const NummiBigImg = 'https://www.nummirock.fi/2017/images/Etusivu_tausta_2021_desktop-C.jpg';
+const NummiBigTxt = 'https://nummirock.fi/2017/images/otsikko-PVM-levea-2021C.png';
 
 const useStyles = makeStyles(theme => ({
     wallPaperContainer: {
         height: "100vh",
         backgroundColor: theme.palette.background
-
     },
     wallPaper: {
         animation: `$fadein 2s`,
         height: "100%",
         width: "100%",
         objectFit: "cover",
+    },
+    wallPaperText: {
+        position: "absolute",
+        bottom: "0",
+        height: "40%",
     },
     "@keyframes fadein": {
         "0%": {
@@ -31,8 +36,9 @@ function LandingSection() {
     return (
         <Grid container item className={classes.wallPaperContainer}>
             <Grid item xs={false} md={1}></Grid>
-            <Grid item container xs={12} md={10}>
+            <Grid item container xs={12} md={10} direction="column" alignItems="center">
                 <Img src={NummiBigImg} className={classes.wallPaper} alt="Nummirock" />
+                <img src={NummiBigTxt} className={classes.wallPaperText} alt="" />
             </Grid>
             <Grid item xs={false} md={1}></Grid>
         </Grid >);
